afa7ada4e8276b356117ab1302dfb43426cc5926
Divyansh Singh <divyanshsingh@Divyanshs-MacBook-Air.local>
2025-02-17 03:02:27 +0530

    cursor

--- colorchange.js
+++ colorchange.js
@@ -7,14 +7,48 @@ const randomColor=function(){
     return color;
 };
 let stoped;
+
 const startChange=function(){
-    stoped= setInterval(changeBg,50)
-function changeBg(){   document.body.style.backgroundColor=randomColor();
+    if(!stoped){
+        stoped= setInterval(changeBg,50)
+    }
+function changeBg(){   
+    document.body.style.backgroundColor=randomColor();
 
 }
 }
 const stopChange=function(){
     clearInterval(stoped)
+    stoped=null;
 }
 document.querySelector("#start").addEventListener('click',startChange);
 document.querySelector("#stop").addEventListener('click',stopChange);
+
+
+
+const customCursor = document.getElementById('custom-cursor');
+
+let mouseX = 0, mouseY = 0;
+let cursorX = 0, cursorY = 0;
+
+document.addEventListener('mousemove', (e) => {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+function moveCursor() {
+    const dx = mouseX - cursorX;
+    const dy = mouseY - cursorY;
+
+    cursorX += dx * 0.3; 
+    cursorY += dy * 0.3;
+
+    customCursor.style.left = `${cursorX}px`;
+    customCursor.style.top = `${cursorY}px`;
+
+    requestAnimationFrame(moveCursor);
+}
+
+moveCursor();
+
+
